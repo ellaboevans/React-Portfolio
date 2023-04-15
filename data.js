98390d2b1d578f20a119522af8ef70cb72ae6172
Evans <ellaboevans@gmail.com>
2023-04-15 13:11:08 +0000

    Add NavItems Objects

---
+++ data.js
@@ -0,0 +1,23 @@
+export const navItems = [
+    {
+        id: 1,
+        name: "Home",
+        link: "/",
+    },
+    {
+        id: 2,
+        name: "About",
+        link: "/about",
+    },
+    {
+        id: 3,
+        name: "Portfolio",
+        link: "/portfolio"
+    },
+    {
+        id: 4,
+        name: "Mentorship",
+        link: "/mentorship"
+    }
+
+]
