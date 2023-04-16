b46bdd59c00d94b5a4cab85964325704f6b82c9b
Evans <ellaboevans@gmail.com>
2023-04-16 02:33:57 +0000

    Add Theme Options

--- data.js
+++ data.js
@@ -38,3 +38,18 @@ export const socialItems = [
     link: "https://www.twitter.com/dev_concept",
   },
 ];
+
+export const options = [
+  {
+    icon: "sunny",
+    text: "light",
+  },
+  {
+    icon: "moon",
+    text: "dark",
+  },
+  {
+    icon: "desktop-outline",
+    text: "system",
+  },
+];
