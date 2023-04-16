da062c9a696ba82c8de235354e48fa15148087ce
Evans <ellaboevans@gmail.com>
2023-04-16 01:02:41 +0000

    Add SocialItems

--- data.js
+++ data.js
@@ -1,23 +1,40 @@
 export const navItems = [
-    {
-        id: 1,
-        name: "Home",
-        link: "/",
-    },
-    {
-        id: 2,
-        name: "About",
-        link: "/about",
-    },
-    {
-        id: 3,
-        name: "Portfolio",
-        link: "/portfolio"
-    },
-    {
-        id: 4,
-        name: "Mentorship",
-        link: "/mentorship"
-    }
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    link: "/about",
+  },
+  {
+    id: 3,
+    name: "Portfolio",
+    link: "/portfolio",
+  },
+  {
+    id: 4,
+    name: "Mentorship",
+    link: "/mentorship",
+  },
+];
 
-]
+export const socialItems = [
+  {
+    id: 1,
+    name: "logo-github",
+    link: "https://www.github.com/ellaboevans",
+  },
+  {
+    id: 2,
+    name: "logo-linkedin",
+    link: "https://www.linkedin.com/in/eelabo/",
+  },
+  {
+    id: 3,
+    name: "logo-twitter",
+    link: "https://www.twitter.com/dev_concept",
+  },
+];
