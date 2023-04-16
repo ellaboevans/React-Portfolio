1c632bebb42e0ef171021d11f51c9f4e4ffa405d
Evans <ellaboevans@gmail.com>
2023-04-16 02:36:28 +0000

    Refactor base

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faMoon } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import { socialItems } from "../../data";
