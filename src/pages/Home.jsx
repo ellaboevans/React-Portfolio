1cdcb2f71a15f132cc98598ef393faab572386f9
Evans <ellaboevans@gmail.com>
2023-04-16 01:12:38 +0000

    Format Code with prettier

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,8 +43,6 @@ function Home() {
     <div className="bg-heroWhite bg-contain bg-blend-mulitply dark:bg-her dark:bg-cover bg-center dark:bg-blend-soft-light z-10 h-screen overflow-x-hidden bg-white dark:bg-slate-800 duration-100">
       <NavBar handleThemeSwitch={handleThemeSwitch} />
       <div className="  flex items-center px-4 md:px-8 jusitfy-between h-screeen py-32 w-full duration-100 my-8 md:my-0">
-
-        
         {/* Left Side Items */}
 
         <div className=" hidden md:flex items-center flex-col space-y-3 mt-28 md:mt-0 px-3 animate-[bounce_4s_infinite] hover:animate-none">
