bec38edcffb565a65e4a80d7abeff5ef298181d7
Evans <ellaboevans@gmail.com>
2023-04-17 04:39:57 +0000

    Import and Add Logos

--- data.js
+++ data.js
@@ -1,3 +1,13 @@
+import nodejs from "./src/assets/nodejs.png";
+import html from "./src/assets/html.png";
+import js from "./src/assets/js.png";
+import css from "./src/assets/css.png";
+import django from "./src/assets/django.png";
+import python from "./src/assets/python.png";
+import react from "./src/assets/react.png";
+import npm from "./src/assets/npm.png";
+import mongodb from "./src/assets/mongodb.png";
+
 export const navItems = [
   {
     id: 1,
@@ -51,5 +61,53 @@ export const options = [
   {
     icon: "desktop-outline",
     text: "system",
+  },
+];
+
+export const sliders = [
+  {
+    id: 1,
+    title: "Node",
+    img: nodejs,
+  },
+  {
+    id: 2,
+    title: "HTML",
+    img: html,
+  },
+  {
+    id: 3,
+    title: "CSS",
+    img: css,
+  },
+  {
+    id: 4,
+    title: "JavaScript",
+    img: js,
+  },
+  {
+    id: 5,
+    title: "React",
+    img: react,
+  },
+  {
+    id: 6,
+    title: "Python",
+    img: python,
+  },
+  {
+    id: 7,
+    title: "Django",
+    img: django,
+  },
+  {
+    id: 8,
+    title: "NPM",
+    img: npm,
+  },
+  {
+    id: 9,
+    title: "MongoDB",
+    img: mongodb,
   },
 ];
