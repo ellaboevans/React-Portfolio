65cad7ee5df3a6db759de30be65c99e550f70f9c
Evans <ellaboevans@gmail.com>
2023-04-26 19:39:06 +0000

    Add Project Module and then render

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,66 +1,47 @@
 import React from "react";
 import Card from "./Card";
+import { projects } from "../../data";
+import { Link } from "react-router-dom";
 
 function Project() {
+  const itemsPerPage = 3;
+  const data = projects;
+
+  // Calculate the start and end index of the items to display
+  const slicedData = data.slice(0, itemsPerPage);
+
   return (
     <section className="w-screen py-8 dark:bg-slate-800">
-      <div className="flex justify-evenly items-center">
-        <div className="hidden md:block h-[2px] w-96 dark:bg-slate-400 bg-slate-800"></div>
-        <p className="dark:text-gray-200 uppercase text-slate-700 text-[22px] font-semibold">
-          Some of my latest work
-        </p>
-        <div className=" hidden md:block h-[2px] w-96 dark:bg-slate-400 bg-slate-800"></div>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 items-center px-12 md:px-24 py-6 gap-4">
-        {/* Card Components */}
-        <Card
-          img="https://cdn.dribbble.com/userupload/4173059/file/original-1aebd83221dfcb583799b43de30c4621.png?compress=1&resize=320x240&vertical=top"
-          title="Project Name"
-          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ipsa
-        earum pariatur nam asperiores dicta corporis cupiditate! Eveniet, alias
-        excepturi."
-          tag="UX Design"
-        />
-        <Card
-          img="https://cdn.dribbble.com/userupload/4396498/file/original-7f018dd4f3260b5099adc5fd8da28dee.png?compress=1&resize=1024x768"
-          title="Project Name"
-          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ipsa
-        earum pariatur nam asperiores dicta corporis cupiditate! Eveniet, alias
-        excepturi."
-          tag="Mobile App"
-        />
-        <Card
-          img="https://cdn.dribbble.com/userupload/4962756/file/original-732a558b1b3ae95a7551f61a5c0a9b47.png?compress=1&resize=400x300&vertical=top"
-          title="Project Name"
-          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ipsa
-        earum pariatur nam asperiores dicta corporis cupiditate! Eveniet, alias
-        excepturi."
-          tag="UI Design"
-        />
-        <Card
-          img="https://cdn.dribbble.com/userupload/5223146/file/original-01cba0b74c69d26af2ff386a3824fcbe.png?compress=1&resize=320x240&vertical=top"
-          title="Project Name"
-          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ipsa
-        earum pariatur nam asperiores dicta corporis cupiditate! Eveniet, alias
-        excepturi."
-          tag="Web Design"
-        />
-        <Card
-          img="https://cdn.dribbble.com/userupload/4823854/file/original-7658742d626e1486fdfa889f556eb90a.png?compress=1&resize=320x240&vertical=top"
-          title="Project Name"
-          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ipsa
-        earum pariatur nam asperiores dicta corporis cupiditate! Eveniet, alias
-        excepturi."
-          tag="Frontend Dev."
-        />
-        <Card
-          img="https://cdn.dribbble.com/userupload/4399707/file/original-be5d0f58a9efd912c1cd721ee42c8ebb.png?compress=1&resize=320x240&vertical=top"
-          title="Project Name"
-          description=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas ipsa
-        earum pariatur nam asperiores dicta corporis cupiditate! Eveniet, alias
-        excepturi."
-          tag=""
-        />
+      <div>
+        <div className="flex justify-evenly items-center">
+          <div className="hidden md:block h-[2px] w-96 dark:bg-slate-400 bg-slate-800"></div>
+          <p className="dark:text-gray-200 uppercase text-slate-700 text-[22px] font-semibold">
+            Some of my latest work
+          </p>
+          <div className=" hidden md:block h-[2px] w-96 dark:bg-slate-400 bg-slate-800"></div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 justify-center items-center px-12 md:px-24 py-6 gap-4">
+          {/* Card Components */}
+          {slicedData.map((project) => (
+            <Card
+              key={project.id}
+              img={project.img}
+              title={project.title}
+              description={project.description}
+              tag={project.tag}
+            />
+          ))}
+        </div>
+        <div className="w-36 text-center mx-auto items-center">
+          <Link to="/portfolio">
+            <p
+              className="dark:text-white bg-gray-200 dark:bg-slate-700 py-2 px-4 mt-8 dark:hover:bg-slate-500 rounded-md hover:bg-red-500 transition hover:text-white cursor-pointer
+            "
+            >
+              View More
+            </p>
+          </Link>
+        </div>
       </div>
     </section>
   );
