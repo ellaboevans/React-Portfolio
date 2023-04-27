b9b76e70f5861b8cf4502a6fc709635145290599
Evans <ellaboevans@gmail.com>
2023-04-27 23:27:10 +0000

    Updates React Router Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
@@ -9,15 +9,26 @@ import ErrorPage from "./pages/ErrorPage";
 function App() {
   return (
     <div className="dark:bg-slate-800 duration-100">
-      <Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={Home} path="/" exact />
+          <Route Component={About} path="/about" />
+          <Route Component={Portfolio} path="/portfolio" />
+          <Route Component={Mentorship} path="/mentorship" />
+          <Route Component={ErrorPage} path="*" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+{
+  /* <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/mentorship" element={<Mentorship />} />
         <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </div>
-  );
+      </Routes> */
 }
-
 export default App;
