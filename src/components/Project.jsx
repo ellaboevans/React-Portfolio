4fa745b660ffcb57b86d8c9fec2144de175726e2
Evans <ellaboevans@gmail.com>
2023-05-01 14:38:06 +0000

    Removing unwanted items

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import Card from "./Card";
 import { Link } from "react-router-dom";
 import createClient from "../client";
 import SkeletonElement from "../skeletons/SkeletonElement";
