ec9438e1e17824d7a28f599fd46d58845f3e2788
Evans <ellaboevans@gmail.com>
2023-05-02 22:13:46 +0000

    Setting up Team Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Mentorship from "./pages/Mentorship";
 import ErrorPage from "./pages/ErrorPage";
+import Team from "./pages/Team";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route Component={About} path="/about" />
           <Route Component={Portfolio} path="/portfolio" />
           <Route Component={Mentorship} path="/mentorship" />
+          <Route Component={Team} path="/team" />
           <Route Component={ErrorPage} path="*" />
         </Routes>
       </BrowserRouter>
