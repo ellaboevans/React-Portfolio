b1c9c8fce3bb9b40f4cb971027fbd461283208fb
Evans <ellaboevans@gmail.com>
2023-05-05 17:25:31 +0000

    Building Components

--- src/components/BootCampItems.jsx
+++ src/components/BootCampItems.jsx
@@ -1,89 +1,38 @@
 import React, { useEffect, useState } from "react";
 import createClient from "../client";
+import SkeletonElement from "../skeletons/SkeletonElement";
 
 function BootCampItems() {
-  const [bootCamps, setBootCamps] = useState([
-    {
-      id: 1,
-      image:
-        "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
-      title: "Adobe Photoshop Training",
-      instructor: "Evans Elabo",
-      date: "Oct 14, 2021",
-      time: "16:00 GMT",
-      link: "/bootcamp/form",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
-    },
-
-    {
-      id: 2,
-      image:
-        "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
-      title: "Adobe Photoshop Training",
-      instructor: "Evans Elabo",
-      date: "Oct 14, 2021",
-      time: "16:00 GMT",
-      link: "/bootcamp/form",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
-    },
-
-    {
-      id: 3,
-      image:
-        "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
-      title: "Adobe Photoshop Training",
-      instructor: "Evans Elabo",
-      date: "Oct 14, 2021",
-      time: "16:00 GMT",
-      link: "/bootcamp/form",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
-    },
-    {
-      id: 4,
-      image:
-        "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
-      title: "Adobe Photoshop Training",
-      instructor: "Evans Elabo",
-      date: "Oct 14, 2021",
-      time: "16:00 GMT",
-      link: "/bootcamp/form",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
-    },
-    {
-      id: 5,
-      image:
-        "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
-      title: "Adobe Photoshop Training",
-      instructor: "Evans Elabo",
-      date: "Oct 14, 2021",
-      time: "16:00 GMT",
-      link: "/bootcamp/form",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
-    },
-    {
-      id: 6,
-      image:
-        "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
-      title: "Adobe Photoshop Training",
-      instructor: "Evans Elabo",
-      date: "Oct 14, 2021",
-      time: "16:00 GMT",
-      link: "/bootcamp/form",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
-    },
-  ]);
+  const [bootCamps, setBootCamps] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await createClient.fetch(`
+      try {
+        const data = await createClient.fetch(`
+        *[_type == "bootcamp"]{
+          title,
+          description,
+          date,
+          time,
+          instructor,
+          link,
+          bootcampImage{
+            asset->{
+              _id,
+              url
+            }
+          },
+        }
         `);
+        setBootCamps(data);
+      } catch (error) {
+        console.log(`Error feetching data: ${error}`);
+      }
     };
+    const timer = setTimeout(() => {
+      fetchData();
+    }, 2000);
+    return () => clearTimeout(timer);
   }, []);
 
   return (
@@ -102,47 +51,60 @@ function BootCampItems() {
         </p>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 items-center px-12 md:px-24 py-6 gap-10">
-        {bootCamps.map((bootCamp) => (
-          <div
-            className="  p-3 dark:bg-slate-800 shadow rounded dark:hover:outline outline-1 dark:outline-slate-400 hover:shadow-lg duration-300 hover:scale-105 hover:shadow-xl"
-            key={bootCamp.id}
-          >
-            <div>
-              <img
-                src={bootCamp.image}
-                className="w-[100%] object-cover  rounded"
-              />
-            </div>
-            <div className="mt-2 ">
-              <p className=" text-sky-700 text-md dark:text-white font-bold rounded mt-4">
-                {bootCamp.date} {bootCamp.time}
+        {!bootCamps &&
+          [1, 2, 3, 4, 5, 6].map((n) => <SkeletonElement key={n} />)}
+        {bootCamps &&
+          bootCamps.map((bootCamp, index) => (
+            <div
+              className=" w-[380px] p-3 dark:bg-slate-800 shadow rounded dark:hover:outline outline-1 dark:outline-slate-400 hover:shadow-lg duration-300 hover:scale-105 hover:shadow-xl"
+              key={index}
+            >
+              <div>
+                <img
+                  src={
+                    bootCamp.bootcampImage?.asset.url
+                      ? bootCamp.bootcampImage?.asset.url
+                      : "https://t4.ftcdn.net/jpg/03/56/02/11/240_F_356021129_xu10B4UK64NAxI17YyXq3MRiKIV0R93E.jpg"
+                  }
+                  alt={bootCamp.title}
+                  className=" h-full object-cover  rounded"
+                />
+              </div>
+              <div className="mt-2 ">
+                <p className=" text-sky-700 text-md uppercase dark:text-white font-bold rounded mt-4">
+                  {bootCamp.date ? bootCamp.date : "date not set"}{" "}
+                  <span className="float-right">
+                    {bootCamp.time ? bootCamp.time : "time not set"}
+                  </span>
+                </p>
+                <p className="dark:text-gray-200 text-slate-700 font-semibold text-[22px] my- capitalize">
+                  {bootCamp.title ? bootCamp.title : "Bootcamp Title"}
+                </p>
+              </div>
+              <p className="text-gray-400 mb-3 leading-normal">
+                {bootCamp.description
+                  ? bootCamp.description
+                  : "Code Concept is setting up something huge for you guys. It will be a win, win for us all. Just stay tuned!!! We are definitely going to take you from zero to hero."}
               </p>
-              <p className="dark:text-gray-200 text-slate-700 font-semibold text-[22px] my- capitalize">
-                {bootCamp.title}
-              </p>
+              <div className="flex items-center  ">
+                <p className="text-gray-400 leading-normal flex-1">
+                  By:{" "}
+                  <span className="text-sky-700 font-bold uppercase">
+                    {" "}
+                    {bootCamp.instructor ? bootCamp.instructor : "dev_concept"}
+                  </span>
+                </p>
+                <a
+                  href={bootCamp.link}
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="float-right p-2 bg-sky-600 text-gray-100 font-semibold text-sm  duration-300 uppercase"
+                >
+                  Register
+                </a>
+              </div>
             </div>
-            <p className="text-gray-400 mb-3 leading-normal">
-              {bootCamp.description}
-            </p>
-            <div className="flex items-center  ">
-              <p className="text-gray-400 leading-normal flex-1">
-                By:{" "}
-                <span className="text-sky-700 font-bold">
-                  {" "}
-                  {bootCamp.instructor}
-                </span>
-              </p>
-              <a
-                href={bootCamp.link}
-                target="_blank"
-                rel="noreferrer noopener"
-                className="float-right p-2 bg-sky-600 text-gray-100 font-semibold text-sm  duration-300 uppercase"
-              >
-                Register
-              </a>
-            </div>
-          </div>
-        ))}
+          ))}
       </div>
       {/* Pagination */}
       {/* <div className="w-[300px] flex justify-center py-2 items-center mx-auto space-x-6">
@@ -167,3 +129,77 @@ function BootCampItems() {
 }
 
 export default BootCampItems;
+
+// {
+//       id: 1,
+//       image:
+//         "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
+//       title: "Adobe Photoshop Training",
+//       instructor: "Evans Elabo",
+//       date: "Oct 14, 2021",
+//       time: "16:00 GMT",
+//       link: "/bootcamp/form",
+//       description:
+//         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
+//     },
+
+//     {
+//       id: 2,
+//       image:
+//         "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
+//       title: "Adobe Photoshop Training",
+//       instructor: "Evans Elabo",
+//       date: "Oct 14, 2021",
+//       time: "16:00 GMT",
+//       link: "/bootcamp/form",
+//       description:
+//         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
+//     },
+
+//     {
+//       id: 3,
+//       image:
+//         "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
+//       title: "Adobe Photoshop Training",
+//       instructor: "Evans Elabo",
+//       date: "Oct 14, 2021",
+//       time: "16:00 GMT",
+//       link: "/bootcamp/form",
+//       description:
+//         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
+//     },
+//     {
+//       id: 4,
+//       image:
+//         "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
+//       title: "Adobe Photoshop Training",
+//       instructor: "Evans Elabo",
+//       date: "Oct 14, 2021",
+//       time: "16:00 GMT",
+//       link: "/bootcamp/form",
+//       description:
+//         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
+//     },
+//     {
+//       id: 5,
+//       image:
+//         "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
+//       title: "Adobe Photoshop Training",
+//       instructor: "Evans Elabo",
+//       date: "Oct 14, 2021",
+//       time: "16:00 GMT",
+//       link: "/bootcamp/form",
+//       description:
+//         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci minus consequuntur impedit vero est dolorem modi rerum sit animi. Quia tenetur, ex nisi molestias perferendis",
+//     },
+//     {
+//       id: 6,
+//       image:
+//         "https://pbs.twimg.com/media/FASXUTCXEAMQvwb?format=jpg&name=medium",
+//       title: "Adobe Photoshop Training",
+//       instructor: "Evans Elabo",
+//       date: "Oct 14, 2021",
+//       time: "16:00 GMT",
+//       link: "",
+//       description: "",
+//     },
