a4b722bd72094ecd12864f5fef2003cf10946170
Evans <ellaboevans@gmail.com>
2023-05-06 22:56:06 +0000

    Changing Resume Link

--- src/components/ResumeBtn.jsx
+++ src/components/ResumeBtn.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function ResumeBtn() {
   return (
     <>
       <div className="my-5">
         <a
-          href="https://docs.google.com/document/d/1-I3gFV0nVhNoS-itnti1TeZJFrCxEilgrX2b_Bdo3qw/edit?usp=sharing"
+          href="https://drive.google.com/file/d/1NrZwufcDDmitVeMrymndCkXJ6IAGIEUC/view?usp=sharing"
           target="_blank"
           className="dark:text-white bg-gray-200 dark:bg-slate-700 py-2 px-4 mt-8 dark:hover:bg-slate-500 rounded-md hover:bg-sky-600 transition hover:text-white cursor-pointer
             "
@@ -17,4 +17,4 @@ function ResumeBtn() {
   );
 }
 
-export default ResumeBtn
+export default ResumeBtn;
